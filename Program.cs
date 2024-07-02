4c9de44b74470f2b436196bd3a8dc9a91965636a
mouravids <kgvfx13@gmail.com>
2024-07-02 13:33:25 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Tickets;
+
+namespace proyectoTicket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ticket oTicket = new Ticket();
+            oTicket.setColor("Naranja");
+            oTicket.setTamanio(2);
+            oTicket.setTipo(3);
+            oTicket.setCantidad(120);
+
+            // Consola
+
+            Console.WriteLine("Color de la pelota: " + oTicket.getColor());
+            Console.WriteLine("Tipo: " + oTicket.getTipo());
+            Console.WriteLine("El pago total es: " + oTicket.calcularPrecio());
+        }
+    }
+}
---
+++ Ticket.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tickets
+{
+   public class Ticket
+    {
+        private int tipo;
+        private string color;
+        private double tamanio;
+        private int cantidad;
+
+
+        // Tipo
+        public int getTipo()
+        {
+            return tipo;
+        }
+
+        public void setTipo(int tipo)
+        {
+            if (tipo == 1 || tipo == 2 || tipo == 3)
+            {
+                this.tipo = tipo;
+            }else
+            {
+                this.tipo = 1;
+            }
+        }
+
+        // Color
+
+        public string getColor()
+        {
+            return color;
+        }
+
+        public void setColor(string color)
+        {
+            this.color = color;
+        }
+
+        // Tamaño
+
+        public double getTamanio()
+        {
+            return tamanio;
+        }
+
+        public void setTamanio(double tamanio)
+        {
+            this.tamanio = tamanio;
+        }
+        // Cantidad
+        public int getCantidad()
+        {
+            return cantidad;
+        }
+
+        public void setCantidad(int cantidad)
+        {
+            if (cantidad > 0)
+            {
+                this.cantidad = cantidad;
+            }else
+            {
+                this.cantidad = 1;
+            }
+        }
+
+        // Calcular Tipo
+        public int calcularTipo()
+        {
+            int tipo = this.tipo;
+            switch (tipo)
+            {
+                case 1:
+                    return 5;
+                case 2:
+                    return 8;
+                case 3:
+                    return 10;
+
+                default:
+                    return 0;
+            }
+        }
+
+
+        // Calcular Precio
+        public double calcularPrecio()
+        {
+            int resultado = calcularTipo();
+            return resultado * this.cantidad;
+        }
+    }
+}
